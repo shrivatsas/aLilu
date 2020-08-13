8a1debf97f515155106dd19937e39d1430f250b3
Shrivatsa <shrivatsa.swadi@gmail.com>
2020-08-13 10:07:58 +0530

    echo service up

--- project.clj
+++ project.clj
@@ -3,7 +3,16 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [aleph "0.4.6"]
+                ;  [org.apache.logging.log4j/log4j "2.13.3" :extension "pom"
+                ;                   :exclusions [javax.mail/mail
+                ;                               javax.jms/jms
+                ;                               com.sun.jdmk/jmxtools
+                ;                               com.sun.jmx/jmxri]]
+                                              ]
   :main ^:skip-aot alilu.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
+
+  ; [org.apache.logging.log4j/log4j "2.13.3" :extension "pom"]
--- src/alilu/core.clj
+++ src/alilu/core.clj
@@ -1,7 +1,22 @@
 (ns alilu.core
+  (:require [aleph.tcp :as tcp]
+            [manifold.stream :as s]
+            [clojure.tools.logging :as log])
   (:gen-class))
+
+(defn echo-handler [s info]
+  (s/connect s s))
+  
+(defn start-server [port]
+  (tcp/start-server echo-handler
+    {:port port} )
+  (log/info "Listening.."))
+
+(defn start-up []
+  (start-server 10001))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (start-up))
